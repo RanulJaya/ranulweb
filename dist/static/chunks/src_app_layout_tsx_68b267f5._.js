a0b753fa826b1c4dc750f1ba11fa99b66f7a9841
RanulJaya <ranul.jaya@gmail.com>
2025-07-20 19:02:23 +1200

    removed dist folder

--- dist/static/chunks/src_app_layout_tsx_68b267f5._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_next_0766a282._.js",
-  "static/chunks/src_app_35213f52._.js",
-  "static/chunks/src_app_user_module_afdb114e.css"
-],
-    source: "dynamic"
-});
