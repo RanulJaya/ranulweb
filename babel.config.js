4aade424f5a107708954ed637c930825d22972df
RanulJaya <ranul.jaya@gmail.com>
2025-07-20 23:40:49 +1200

    testing env

---
+++ babel.config.js
@@ -0,0 +1,10 @@
+    module.exports = {
+      presets: [
+        ['next/babel', { 'preset-react': { runtime: 'automatic' } }], // For Next.js projects
+        // or for general React projects:
+        // '@babel/preset-env',
+        // '@babel/preset-react',
+      ],
+      // Add any other necessary plugins here
+      // plugins: [],
+    };
