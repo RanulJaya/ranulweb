32142ff0ca0b4ad0aa455008e6de21d19760a95a
RanulJaya <ranul.jaya@gmail.com>
2025-07-22 00:10:19 +1200

    removed test folder

--- __test__/babel.config.js
+++
@@ -1,7 +0,0 @@
-    export const presets = [
-  ['next/babel', { 'preset-react': { runtime: 'automatic' } }],
-  // For Next.js projects
-  // or for general React projects:
-  // '@babel/preset-env',
-  // '@babel/preset-react',
-];
